400937858a57b2de821d3498a5361dd235fd1f0e
Ronaldoball <xdduana@163.com>
2019-11-29 14:42:31 +0800

    1.add the scrollbar style

--- my-blog/src/App.vue
+++ my-blog/src/App.vue
@@ -17,6 +17,26 @@ export default {
   padding: 0;
 }
 
+div::-webkit-scrollbar{
+  width:10px;
+  height:10px;
+  /**/
+}
+div::-webkit-scrollbar-track{
+  background: rgb(239, 239, 239);
+  border-radius:2px;
+}
+div::-webkit-scrollbar-thumb{
+  background: #bfbfbf;
+  border-radius:10px;
+}
+div::-webkit-scrollbar-thumb:hover{
+  background: #333;
+}
+div::-webkit-scrollbar-corner{
+  background: #179a16;
+}
+
 /* .site-header {
   position: absolute;
   left: 0;
