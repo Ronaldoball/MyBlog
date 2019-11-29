8de8679b5fd155b36bf5d5d0135dc3ea48012b6e
Ronaldoball <xdduana@163.com>
2019-11-29 14:44:36 +0800

    1.add the route

--- my-blog/src/router/index.js
+++ my-blog/src/router/index.js
@@ -1,45 +1,53 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/views/Home'
-import Login from '@/views/Login'
-import Sign from '@/views/Sign'
-import Editor from '@/views/Editor'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home";
+import Login from "@/views/Login";
+import Sign from "@/views/Sign";
+import Editor from "@/views/Editor";
+import BlogContent from "@/views/BlogContent";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      redirect: '/login'
+      path: "/",
+      redirect: "/login"
     },
     {
-      path: '/login',
-      name: 'Login',
+      path: "/login",
+      name: "Login",
       components: {
         main: Login
       }
     },
     {
-      path: '/sign',
-      name: 'Sign',
+      path: "/sign",
+      name: "Sign",
       components: {
         main: Sign
       }
     },
     {
-      path: '/index',
-      name: 'Index',
+      path: "/index",
+      name: "Index",
       components: {
         main: Home
       }
     },
     {
-      path: '/editor',
-      name: 'Editor',
+      path: "/editor",
+      name: "Editor",
       components: {
         main: Editor
       }
+    },
+    {
+      path: "/blogContent",
+      name: "BlogContent",
+      components: {
+        main: BlogContent
+      }
     }
   ]
-})
+});
